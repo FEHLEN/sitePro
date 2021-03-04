34014cf6813c02a155b9a802a168f9965b3e4b5f
Fehlen <fehlenlili64100@gmail.com>
2021-03-04 10:18:06 +0100

    mise à jour de tous les fichiers

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,10 @@ const app = express();
 const {verify} = require('./middleware/auth.js');
 
 const {login, refresh} = require('./controllers/login');
-const routeHandler = require('./controllers/produits');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.post('/login', login);
 app.post('/refresh', refresh);
 
-app.get('/produits', verify, routeHandler);
+app.get('/comments', verify, routeHandler);
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,11 +6,11 @@ exports.verify = function(req, res, next){
     if (!accessToken){
         return res.status(403).send()
     }
-    let payload;
+    let token;
     try{
         //use the jwt.verify method to verify the access token
         //throws an error if the token has expired or has a invalid signature
-        payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        token = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
         next()
     }
     catch(e){
